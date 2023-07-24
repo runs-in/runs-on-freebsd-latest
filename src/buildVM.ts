6cdd90ad541f9527d12a9030242bb5379fe74c0b
Jacob Hummer <jcbhmr@outlook.com>
2023-07-24 10:14:50 -0500

    idea

---
+++ src/buildVM.ts
@@ -0,0 +1,15 @@
+import { $ } from "execa";
+
+async function buildVM() {
+  
+}
+
+async function cacheHasBuiltVM() {}
+
+async function restoreBuiltVM() {}
+
+async function cacheBuiltVM() {}
+
+
+export default buildVM;
+export { restoreBuiltVM, cacheBuiltVM, cacheHasBuiltVM };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,79 @@
 #!/usr/bin/env -S deno run -A
-import * as core from "npm:@actions/core@1.10.0";
-import * as cache from "npm:@actions/cache@3.2.1";
-import { $ } from "npm:execa@7.1.1";
-import * as YAML from "npm:yaml@2.3.1";
+import * as core from "@actions/core";
+import * as YAML from "yaml";
+import { type, arrayOf } from "arktype";
+import buildVM, {
+  cacheBuiltVM,
+  cacheHasBuiltVM,
+  restoreBuiltVM,
+} from "./buildVM.ts";
 
-const runsOn = core.getInput("runs_on");
-const steps = YAML.parse(core.getInput("steps"));
+async function withGroup<A extends any[], R>(
+  name: string,
+  block: (...a: A) => Promise<R>,
+  ...args: A
+): Promise<R> {
+  core.startGroup(name);
+  try {
+    return await block(...args);
+  } finally {
+    core.endGroup();
+  }
+}
 
-core.startGroup("📥 Downloading VM")
+const stepsT = arrayOf(
+  type({
+    "id?": "string",
+    "if?": "boolean|string",
+    "name?": "string",
+    "uses?": "string",
+    "run?": "string",
+    "shell?": "string",
+    "with?": "object",
+    "env?": "object",
+    "continue-on-error?": "boolean|string",
+    "timeout-minutes?": "number|string",
+  })
+);
+
+function throwIfInput(name: string): void {
+  if (core.getInput(name)) {
+    throw new DOMException(
+      `Input ${name} is not supported. Did you mean to specify this at the ` +
+        `job level? For more information on supported options, check out the ` +
+        `GitHub readme: https://github.com/runs-on/freebsd-latest#readme`,
+      "SyntaxError"
+    );
+  }
+}
+
+throwIfInput("name");
+throwIfInput("permissions");
+throwIfInput("needs");
+throwIfInput("if");
+throwIfInput("runs_on");
+throwIfInput("environmment");
+throwIfInput("concurrency");
+throwIfInput("outputs");
+throwIfInput("env");
+throwIfInput("defaults");
+// prettier-ignore
+const steps = stepsT.assert(YAML.parse(core.getInput("steps", { required: true })));
+throwIfInput("timeout_minutes");
+throwIfInput("strategy");
+throwIfInput("continue_on_error");
+throwIfInput("container");
+throwIfInput("services");
+throwIfInput("uses");
+throwIfInput("with");
+throwIfInput("secrets");
+
+await withGroup("🚚 Preparing FreeBSD runtime", async () => {
+  if (await cacheHasBuiltVM()) {
+    await withGroup("⚡ Using cached FreeBSD VM", restoreBuiltVM);
+  } else {
+    await withGroup("🔨 Building FreeBSD VM", buildVM);
+  }
+  await withGroup("⬆️ Updating cached FreeBSD VM", cacheBuiltVM);
+});
+await withGroup("🌟 main 🌟", async () => {});
